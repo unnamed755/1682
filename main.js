d443e6e37404d82a899513ca46c36e34e51318d8
unnamed755 <xoliboyevdoniyor@gmail.com>
2024-11-09 14:14:10 +0500

    bot

---
+++ main.js
@@ -0,0 +1,11 @@
+const userName = prompt("Ismingizni kiriting")
+const userAge = +prompt ("id ingizni kiriting" )
+const surName = +prompt ("pasword dingizni kiriting")
+
+const infol ={
+    name:userName,
+    id:userAge,
+    pasword:surName,
+}
+
+console.log(infol);
